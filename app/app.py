0d46ade498a5046ea9eda59e9702a9abd921a3d0
Iris <idbontekoe@hotmail.com>
2024-11-06 15:38:24 +0100

    Updates to unittests

--- app/app.py
+++ app/app.py
@@ -7,10 +7,23 @@ import matplotlib.pyplot as plt
 # import the function get_GDF from the file "./app/getGeoDataFrame.py"
 from app.getGeoDataFrame import get_GDF 
 
+# showcase injecting App settings (parameter `year`)
+from dataclasses import dataclass
+@dataclass
+class AppConfig:
+    year: int
+
 class App(object):
 
     def __init__(self, moveapps_io):
         self.moveapps_io = moveapps_io
+
+    # showcase injecting App settings (parameter `year`)
+    @staticmethod
+    def map_config(config: dict):
+        return AppConfig(
+            year=config["year"] if "year" in config else 1994
+        )
 
     @hook_impl
     def execute(self, data: TrajectoryCollection, config: dict) -> TrajectoryCollection:
@@ -20,11 +33,12 @@ class App(object):
         """Your app code goes here"""
         
         # showcase injecting App settings (parameter `year`)
+        app_config = self.map_config(config=config)
         data_gdf = get_GDF(data) # translate the TrajectoryCollection to a GeoDataFrame
-        logging.info(f'Subsetting data for {config["year"]}')
+        logging.info(f'Subsetting data for {app_config.year}')
         # subset the data to only contain the specified year
-        if config["year"] in data_gdf.index.year:
-            result = data_gdf[data_gdf.index.year==config["year"]]
+        if app_config.year in data_gdf.index.year:
+            result = data_gdf[data_gdf.index.year==app_config.year]
         else:
             result = None
         
@@ -38,9 +52,9 @@ class App(object):
             logging.warning("Nothing to plot")
         
         # showcase accessing auxiliary files
-        auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
-        with open(auxiliary_file_a, 'r') as f:
-            logging.info(f.read())
+        #auxiliary_file_a = MoveAppsIo.get_auxiliary_file_path("auxiliary-file-a")
+        #with open(auxiliary_file_a, 'r') as f:
+        #    logging.info(f.read())
             
         # Translate the result back to a TrajectoryCollection
         if result is not None:
@@ -50,8 +64,6 @@ class App(object):
                 t=data.to_point_gdf().index.name,
                 crs=data.get_crs()
                 )
-        else:
-            result = data
-
+        logging.info(result)
         # return the resulting data for next Apps in the Workflow
         return result
